fb3e49fd0add3ed592e37e54d37514c330a35169
Jay Mensink <j.mensink0206@gmail.com>
2025-07-10 13:45:20 +0200

    Updated privacy agreement

--- applicatie/privacy/index.php
+++ applicatie/privacy/index.php
@@ -11,7 +11,37 @@
         <h1>Privacyverklaring</h1>
         <?php require __DIR__ . '/../common/elem/header.php' ?>
         
-		<p>Hier hoort een verplichte privacyverklaring te staan (GDPR)</p>
+		<p>Uw privacy is voor ons van groot belang. In deze privacyverklaring leggen wij daarom uit hoe wij omgaan met uw persoonsgegevens en wie welke gegevens kan inzien.</p>
+
+        <h2>Verzamelde Gegevens</h2>
+        <p>Door gebruik te maken van onze website, bent u het eens met de verzameling van de volgende van uw gegevens:</p>
+        <ul>
+            <li>Gebruikersnaam</li>
+            <li>Wachtwoord</li>
+            <li>Voornaam</li>
+            <li>Achternaam</li>
+            <li>Adres</li>
+            <li>Bestelde producten</li>
+        </ul>
+
+        <h2>Toegang Gegevens</h2>
+        <p>De toegang tot uw gegevens wordt met zorg behandeld. De volgende gegevens zijn namelijk enkel aan de volgende partijen beschikbaar:</p>
+        <ul>
+            <li>Gebruikersnaam: Openbaar voor alle gebruikers.</li>
+            <li>Wachtwoord: Alleen gehasht zichtbaar voor het accountsysteem.</li>
+            <li>Voornaam: Openbaar voor alle gebruikers.</li>
+            <li>Achternaam: Openbaar voor alle gebruikers.</li>
+            <li>Adres: Alleen zichtbaar voor bedrijfspersoneel en uzelf.</li>
+            <li>Bestelde producten: Alleen zichtbaar voor bedrijfspersoneel en uzelf.</li>
+        </ul>
+
+        <h2>Verwijderen Gegevens</h2>
+        <p>Als u wil dat uw gegevens verwijderd worden uit ons systeem, kunt u dit volledig zelf regelen. Log in op uw account en navigeer naar uw profiel. Op deze pagina staat een knop genaamd "Verwijder Account". Deze knop voert de volgende acties uit binnen ons systeem:</p>
+        <ol>
+            <li>Al uw bestellingen worden losgekoppeld van uw account. De bestelling zelf (incl. bestelde producten en adres) is nog steeds beschikbaar voor ons personeel, maar deze gegevens zijn niet meer terug te koppelen aan uw account.</li>
+            <li>Uw account (incl. gebruikersnaam, wachtwoord-hash, voornaam, achternaam en adres) wordt verwijderd uit ons systeem.</li>
+        </ol>
+    
     </body>
 </html>
 
